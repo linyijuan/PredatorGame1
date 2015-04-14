6cf829cc062ec1d5edc0750f2028b7cb451ccafa
Marcus <marcustan03@yahoo.com.sg>
2015-04-14 13:24:59 +0800

    doc a bit

--- core/src/sutd/istd/groupzero/screens/GameScreen.java
+++ core/src/sutd/istd/groupzero/screens/GameScreen.java
@@ -43,6 +43,7 @@ public class GameScreen implements Screen{
 
 	@Override
     public void render(float delta) {
+        // if block to ensure that both players start at the same time and not lag behind
         if (!actionResolver.didYouStart()){
             Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
             batcher.begin();
@@ -87,7 +88,8 @@ public class GameScreen implements Screen{
 
     @Override
     public void dispose() {
-        renderer.music.stop();
+
+
     }
 
 }
